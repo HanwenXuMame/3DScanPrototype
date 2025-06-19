71cc0b4333055b03790c3bd4f939a313a184671f
HanwenXuMame <128509287+HanwenXuMame@users.noreply.github.com>
2025-06-18 21:30:28 -0400

    fixed rotation

---
+++ 3DScanPrototype/Assets/MVXtoVoxon/Scripts/DebugHotkeys.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DebugHotkeys : MonoBehaviour
+{
+    [System.Serializable]
+    public class HotkeyEvent
+    {
+        public KeyCode key;
+        public UnityEvent onKeyPressed;
+    }
+
+    public List<HotkeyEvent> hotkeyEvents = new List<HotkeyEvent>();
+
+    void Update()
+    {
+        foreach (var hotkey in hotkeyEvents)
+        {
+            if (Input.GetKeyDown(hotkey.key))
+            {
+                hotkey.onKeyPressed?.Invoke();
+            }
+        }
+    }
+}
